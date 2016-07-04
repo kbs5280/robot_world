9f566b6913a3533708f4605a5cc69e8c06bfe338
Kris Sparks <kbs5280@gmail.com>
2016-07-04 17:10:43 -0600

    Feature and model testing updated.

--- test/models/robot_manager_test.rb
+++ test/models/robot_manager_test.rb
@@ -4,25 +4,72 @@ class RobotManagerTest < Minitest::Test
   include TestHelpers
 
   def test_it_creates_a_robot
-    robot_manager.create({"name"=>"Hector",
-                          "city"=>"Juarez",
-                          "state"=>"Mexico",
-                          "avatar"=>"yes",
-                          "birthdate"=>"12/12/12",
-                          "date_hired"=>"12/13/12",
-                          "department"=>"Upper Management"})
+    robot_manager.create({
+                          name: "Hector",
+                          city: "Juarez",
+                          state: "Mexico",
+                          avatar: "yes",
+                          birthdate: "12/12/12",
+                          date_hired: "12/13/12",
+                          department: "Upper Management"
+                        })
 
-    robot = robot_manager.find(robot_manager.all.count)
+    robot = robot_manager.find(robot_manager.all.first["id"])
 
     assert_instance_of Robot, robot
     assert_equal "Hector", robot.name
     assert_equal "Mexico", robot.state
-    binding.pry
   end
 
-  # def test_raw_robots
-  #   robot_manager.raw_robots
-  # end
-  #
+  def test_it_finds_a_robot
+    task1 = robot_manager.create({
+                          name: "Hector",
+                          city: "Juarez",
+                          state: "Mexico",
+                          avatar: "yes",
+                          birthdate: "12/12/12",
+                          date_hired: "12/13/12",
+                          department: "Upper Management"
+    })
+    task2 = robot_manager.create({
+                          name: "Jaime",
+                          city: "Mexico City",
+                          state: "Mexico",
+                          avatar: "yes",
+                          birthdate: "12/14/12",
+                          date_hired: "12/15/12",
+                          department: "Middle Management"
+    })
+
+    robot = robot_manager.find(robot_manager.all.first["id"])
+    assert_equal "Hector", robot.name
+    assert_equal "Upper Management", robot.department
+  end
+
+  def test_it_finds_a_robot
+    task1 = robot_manager.create({
+                          name: "Hector",
+                          city: "Juarez",
+                          state: "Mexico",
+                          avatar: "yes",
+                          birthdate: "12/12/12",
+                          date_hired: "12/13/12",
+                          department: "Upper Management"
+    })
+    task2 = robot_manager.create({
+                          name: "Jaime",
+                          city: "Mexico City",
+                          state: "Mexico",
+                          avatar: "yes",
+                          birthdate: "12/14/12",
+                          date_hired: "12/15/12",
+                          department: "Middle Management"
+    })
+
+    robot = robot_manager.find(robot_manager.all.first["id"])
+    robot_manager.update(robot_manager.all.first["id"], robot)
+  end
+
+
 
 end
